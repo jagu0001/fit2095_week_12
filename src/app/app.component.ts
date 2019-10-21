096795adbbb3bc7be18dfa0e85e78aea3489e4ed
jagu0001 <jagu0001@student.monash.edu>
2019-10-22 03:47:44 +1100

    Initial update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import * as io from "socket.io-client";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'chatapp';
+  messageText: string;
+  senderName: string;
+  messages: Array<any> = [];
+  socket: SocketIOClient.Socket;
+  constructor() {
+    this.socket = io.connect();
+  }
+  ngOnInit() {
+    this.messages = new Array();
+    this.listen2Events();
+  }
+  listen2Events() {
+    this.socket.on("msg", data => {
+      console.log(data);
+      this.messages.push(data);
+    });
+  }
+
+  sendMessage() {
+    let dataToEmit = {
+      name: this.senderName,
+      message: this.messageText
+    }
+    this.socket.emit("newMsg", dataToEmit);
+    this.messageText = "";
+  }
 }
